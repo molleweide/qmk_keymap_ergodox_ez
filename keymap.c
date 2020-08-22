88dfbf363560b9c7cdbe6d79cdeefe853fe7ea48
molleweide <hjalmar.jakobsson@telia.com>
2020-08-22 23:15:44 +0200

    removes unnecessary keymaps

--- keymap.c
+++ keymap.c
@@ -92,15 +92,7 @@ enum layer_names {
     _LMS,
     _RMS,
 
-//    _LCO,
-//    _RCO,
-
     _FUN,
-
-//    _RTE,
-//    _DVO, // rm?
-//    _COL,
-//    _WRK, // rm ?
 
     _MS_TEST, // this is where I do my mouse testing
 };
@@ -180,31 +172,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                         KC_TRNS,        /**/            KC_TRNS,
                                                                         KC_MS_BTN1,     KC_MS_BTN2,     KC_TRNS,        /**/            KC_MS_BTN5,     KC_MS_BTN4,     KC_MS_BTN3),
 
-    /**
-     * MOUSE_HELPER_LR ------------------------------------------------ MOUSE_HELPER_LR ----------------------------------------------- MOUSE_HELPER_LR --------------------------------------------------------------- MOUSE_HELPER_LR ----------------
-     */
-
-//    [_LCO] = LAYOUT_ergodox_pretty(//---|---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       TO(_BAS),
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       KC_MS_BTN3,     xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
-//        xxxxxxxx,       KC_LCTL,        KC_LSFT,        KC_LALT,        KC_LCMD,        KC_MS_BTN4,     /**/            /**/            /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
-//        TO(_BAS),       KC_LEFT,        KC_DOWN,        KC_UP,          KC_RGHT,        KC_MS_BTN5,     xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       KC_MWHL,        /**/            /**/            /**/            /**/            /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
-//        //--------------|***************|***************|***************|***************|---------------$---------------/**/----------------------------$---------------|***************|***************|***************|***************|---------------
-//                                                                                        xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,
-//                                                                        /***************/               xxxxxxxx,       /**/            xxxxxxxx,       /***************/
-//                                                                        KC_MWHD,        KC_MWHU,        KC_MWHR,        /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx),
-//
-//    [_RCO] = LAYOUT_ergodox_pretty(//---|---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       TO(_BAS),
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       KC_MS_BTN3,     xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       /**/            /**/            /**/            KC_MS_BTN4,     KC_RCMD,        KC_RALT,        KC_RSFT,        KC_RCTL,        xxxxxxxx,
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       KC_MS_BTN5,     KC_LEFT,        KC_DOWN,        KC_UP,          KC_RGHT,        TO(_BAS),
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       /**/            /**/            /**/            /**/            /**/            KC_MWHR,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
-//        //--------------|***************|***************|***************|***************|---------------$---------------/**/----------------------------$---------------|***************|***************|***************|***************|---------------
-//                                                                                        xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,
-//                                                                        /***************/               xxxxxxxx,       /**/            xxxxxxxx,       /***************/
-//                                                                        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       /**/            KC_MWHL,       KC_MWHD,       KC_MWHU),
 
     /**
      * FUNCTION_LAYER ------------------------------------------------ FUNCTION_LAYER ----------------------------------------------- FUCNTION_LAYER --------------------------------------------------------------- FUNCTION_LAYER ----------------
@@ -221,58 +188,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                         /***************/               xxxxxxxx,       /**/            xxxxxxxx,       /***************/
                                                                         KC_LALT,        xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,       KC_RALT),
 
-    /**
-     * ROUTER --------------------------------------
-     */
-
-//    [_RTE] = LAYOUT_ergodox_pretty(//---|---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       TO(_BAS),
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       TO(_MS_TEST),   xxxxxxxx,                       /**/                            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
-//        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,                                       /**/                                            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
-//        //--------------|***************|***************|***************|***************|---------------$---------------/**/------------$---------------$---------------|***************|***************|***************|***************|---------------
-//                                                                                        xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,
-//                                                                                                        xxxxxxxx,       /**/            xxxxxxxx,
-//                                                                        xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx),
-
-    /**
-     * Dvorak, Colemak, and Workman ----------------
-     *     these layouts SHOULD BE superimposed nicely on top of the baselayer
-     */
-
-//    [_DVO] = LAYOUT_ergodox_pretty(//---|---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
-//        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        /**/            KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        TO(_BAS),
-//        KC_TRNS,        KC_QUOTE,       KC_COMMA,       KC_DOT,         KC_P,           KC_Y,           KC_TRNS,        /**/            KC_TRNS,        KC_F,           KC_G,           KC_C,           KC_R,           KC_L,           KC_TRNS,
-//        KC_TRNS,        KC_A,           KC_O,           KC_E,           KC_U,          KC_I,                           /**/                            KC_D,           KC_H,           KC_T,           KC_N,           KC_S,           KC_TRNS,
-//        KC_TRNS,        KC_SCOLON,      KC_Q,           KC_J,           KC_K,           KC_X,           KC_TRNS,        /**/            KC_TRNS,        KC_B,           KC_M,           KC_W,           KC_V,           KC_Z,           KC_TRNS,
-//        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,                                        /**/                                            KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
-//        //--------------|***************|***************|***************|***************|---------------$---------------/**/----------------------------$---------------|***************|***************|***************|***************|---------------
-//                                                                                        KC_TRNS,        KC_TRNS,        /**/            KC_TRNS,        KC_TRNS,
-//                                                                                                        KC_TRNS,        /**/            KC_TRNS,
-//                                                                        KC_TRNS,        KC_TRNS,        KC_TRNS,        /**/            KC_TRNS,        KC_TRNS,        KC_TRNS),
-//
-//    [_COL] = LAYOUT_ergodox_pretty(//---|---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
-//        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        /**/            KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        TO(_BAS),
-//        KC_TRNS,        KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,           KC_TRNS,        /**/            KC_TRNS,        KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCOLON,      KC_TRNS,
-//        KC_TRNS,        KC_A,           KC_R,           KC_S,           KC_T,           KC_G,                           /**/                            KC_K,           KC_N,           KC_E,           KC_I,           KC_O,           KC_TRNS,
-//        KC_TRNS,        KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,           KC_TRNS,        /**/            KC_TRNS,        KC_M,           KC_H,           KC_COMMA,       KC_DOT,         KC_SLSH,        KC_TRNS,
-//        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,                                        /**/                                            KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
-//        //--------------|***************|***************|***************|***************|---------------$---------------/**/----------------------------$---------------|***************|***************|***************|***************|---------------
-//                                                                                        KC_TRNS,        KC_TRNS,        /**/            KC_TRNS,        KC_TRNS,
-//                                                                                                        KC_TRNS,        /**/            KC_TRNS,
-//                                                                        KC_TRNS,        KC_TRNS,        KC_TRNS,        /**/            KC_TRNS,        KC_TRNS,        KC_TRNS),
-//
-//    [_WRK] = LAYOUT_ergodox_pretty(//---|---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
-//        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        /**/            KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        TO(_BAS),
-//        KC_TRNS,        KC_Q,           KC_D,           KC_R,           KC_W,           KC_B,           KC_TRNS,        /**/            KC_TRNS,        KC_J,           KC_F,           KC_U,           KC_P,           KC_SLSH,       KC_TRNS,
-//        KC_TRNS,        KC_A,           KC_S,           KC_H,           KC_T,           KC_G,                           /**/                            KC_Y,           KC_N,           KC_E,           KC_O,           KC_I,           KC_TRNS,
-//        KC_TRNS,        KC_Z,           KC_X,           KC_M,           KC_C,           KC_V,           KC_TRNS,        /**/            KC_TRNS,        KC_K,           KC_L,           KC_COMMA,       KC_DOT,         KC_SLSH,       KC_TRNS,
-//        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,                                        /**/                                            KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
-//        //--------------|***************|***************|***************|***************|---------------$---------------/**/----------------------------$---------------|***************|***************|***************|***************|---------------
-//                                                                                        KC_TRNS,        KC_TRNS,        /**/            KC_TRNS,        KC_TRNS,
-//                                                                                                        KC_TRNS,        /**/            KC_TRNS,
-//                                                                        KC_TRNS,        KC_TRNS,        KC_TRNS,        /**/            KC_TRNS,        KC_TRNS,        KC_TRNS),
 
     /**
      * MOUSE TESTING
@@ -374,9 +289,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 //  - mouse
 //    * mouse hook
 //    * regular mouse button
-//  - 12 directions
 //  - create 3-step
-//  -
 
 uint8_t mouseState = 1; // 1 = set magnitude; 2 = set direction
 uint8_t mag = 0;
