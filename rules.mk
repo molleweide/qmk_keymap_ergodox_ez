0992f31f7ec67b2d3ba36a45ddaa53d1bbf761a1
molleweide <hjalmar.jakobsson@telia.com>
2020-09-07 19:26:34 +0200

    fixes .elf linking error > err in rules.mk

--- rules.mk
+++ rules.mk
@@ -1,11 +1,10 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-
-COMMAND_ENABLE = no
-CONSOLE_ENABLE = yes
-DYNAMIC_MACRO_ENABLE = yes
 LINK_TIME_OPTIMIZATION_ENABLE = yes
-ORYX_ENABLE = yes
-POINTING_DEVICE_ENABLE = yes
-TAP_DANCE_ENABLE = yes
+COMMAND_ENABLE = no
 WEBUSB_ENABLE = yes
+ORYX_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+POINTING_DEVICE_ENABLE = yes
+CONSOLE_ENABLE = no
+
