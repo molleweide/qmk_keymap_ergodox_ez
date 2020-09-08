3bd1f8ef71c5d87b72786aa7c7d1134ccf8d67e7
molleweide <hjalmar.jakobsson@telia.com>
2020-09-08 10:01:50 +0200

    cleanup and update names

--- keymap.c
+++ keymap.c
@@ -81,15 +81,15 @@ enum custom_keycodes {
 enum layer_names {
     _BAS = 0,
 
-    _LSY,
-    _RNU,
+    _SYM,
+    _NUM,
 
     _LMS,
     _RMS,
 
     _FUN,
 
-    _MS_TEST, // this is where I do my mouse testing
+    _TEST, // this is where I do my mouse testing
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -99,10 +99,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [_BAS] = LAYOUT_ergodox_pretty(//---|---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
-        TO(_MS_TEST),   KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           WEBUSB,         /**/            xxxxxxxx,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRNS,
+        TO(_TEST),      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           WEBUSB,         /**/            xxxxxxxx,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRNS,
         KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_AU_U,        /**/            KC_MS_WH_UP,    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLSH,
         KC_ESC,         CTL_T(KC_A),    SFT_T(KC_S),    ALT_T(KC_D),    CMD_T(KC_F),    KC_G,           /**/            /**/            /**/            KC_H,           CMD_T(KC_J),    ALT_T(KC_K),    SFT_T(KC_L),    CTL_T(KC_SCOL), KC_QUOTE,
-        TO(_LMS),       LT(_RMS,KC_Z),  LT(_RNU,KC_X),  LT(_FUN,KC_C),  KC_V,           KC_B,           KC_AU_D,        /**/            KC_MS_WH_DOWN,  KC_N,           KC_M,           LT(_FUN,KC_COM),LT(_LSY,KC_DOT),LT(_LMS,KC_SLSH),TO(_RMS),
+        TO(_LMS),       LT(_RMS,KC_Z),  LT(_NUM,KC_X),  LT(_FUN,KC_C),  KC_V,           KC_B,           KC_AU_D,        /**/            KC_MS_WH_DOWN,  KC_N,           KC_M,           LT(_FUN,KC_COM),LT(_SYM,KC_DOT),LT(_LMS,KC_SLSH),TO(_RMS),
         KC_GRAVE,       CTL_T(KC_LEFT), KC_DOWN,        KC_UP,          KC_RGHT,        /*-------------*/               /**/            /*-------------*/               KC_LEFT,        KC_DOWN,        KC_UP,          CTL_T(KC_RGHT), xxxxxxxx,
         //--------------|***************|***************|***************|***************|---------------$---------------/**/----------------------------$---------------|***************|***************|***************|***************|---------------
                                                                                         xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * SYMBOLS / NUMBERS ---------------------------
      */
 
-    [_LSY] = LAYOUT_ergodox_pretty(//---|---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
+    [_SYM] = LAYOUT_ergodox_pretty(//---|---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
         ________,       KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          ________,       /**/            ________,       ________,       ________,       ________,       ________,       ________,       TO(_BAS),
         ________,       KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        ________,       /**/            ________,       ________,       ________,       ________,       ________,       ________,       ________,
         ________,       KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRAVE,                       /**/                            ________,       ________,       ________,       ________,       ________,       ________,
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                         ________,       /**/            ________,
                                                                         ________,       ________,       ________,       /**/            ________,      ________,        ________),
 
-    [_RNU] = LAYOUT_ergodox_pretty(//---|---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
+    [_NUM] = LAYOUT_ergodox_pretty(//---|---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
         oooooooo,       oooooooo,       oooooooo,       oooooooo,       oooooooo,       oooooooo,       oooooooo,       /**/            xxxxxxxx,       KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         TO(_BAS),
         oooooooo,       oooooooo,       oooooooo,       oooooooo,       oooooooo,       oooooooo,       oooooooo,       /**/            xxxxxxxx,       xxxxxxxx,       KC_7,           KC_8,           KC_9,           KC_EQUAL,       KC_F12,
         oooooooo,       oooooooo,       oooooooo,       oooooooo,       oooooooo,       oooooooo,       /**/            /**/            /**/            KC_0,           KC_4,           KC_5,           KC_6,           KC_PLUS,        KC_KP_SLASH,
@@ -182,7 +182,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * MOUSE TESTING
      */
 
-    [_MS_TEST] = LAYOUT_ergodox_pretty(//---------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
+    [_TEST] = LAYOUT_ergodox_pretty(//------------------|---------------X---------------|---------------$---------------/**/------------$---------------|---------------X---------------|---------------|---------------|---------------|---------------
         xxxxxxxx,       MGRID,          MGRID,          MGRID,          MGRID,          xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       TO(_BAS),
         xxxxxxxx,       MGRID,          MGRID,          MGRID,          MGRID,          xxxxxxxx,       xxxxxxxx,       /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
         KC_ESC,         MGRID,          MGRID,          MGRID,          MGRID,          xxxxxxxx,       /**/            /**/            /**/            xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,       xxxxxxxx,
@@ -198,43 +198,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 rgblight_config_t rgblight_config;
 bool disable_layer_color = 0;
 
-uint8_t mouseState = 0; // 0 = set magnitude; 1 = set direction; 2 = enter layer
+uint8_t stateMouseSequence = 0; // 0 = set magnitude; 1 = set direction; 2 = enter layer
 int mag = 0;
 double rad = PI / 180;
 
-void setmd(int m, double d){
-  if (mouseState == 0 || mouseState == 2) {
-    mag = m; mouseState = 1;
+void setp(int m, double d){
+  if (stateMouseSequence == 0 || stateMouseSequence == 2) {
+    mag = m; stateMouseSequence = 1;
   } else {
     report_mouse_t report = pointing_device_get_report();
     report.x = round(mag * cos(d * rad));
     report.y = round(mag * sin(d * rad));
     pointing_device_set_report(report);
     pointing_device_send();
-    mouseState = 0;
+    stateMouseSequence = 0;
   }
 }
 
-void mouse_keys(keyrecord_t *record) { /////////////////////////////////////////////////
+void custom_pointer(keyrecord_t *record) { /////////////////////////////////////////////////
   if (record->event.pressed) { // key pressed
     if (record->event.key.col == 0) {
       switch (record->event.key.row) {
-        case 1: setmd(70, 202.5); break;    case 2: setmd(100, 112.5); break;    case 3: setmd(112, 292.5); break;    case 4:setmd(125,22.5);break;
+        case 1: setp(70, 202.5); break;    case 2: setp(100, 112.5); break;    case 3: setp(112, 292.5); break;    case 4:setp(125,22.5);break;
       }
     }
     if (record->event.key.col == 1) {
       switch (record->event.key.row) {
-        case 1: setmd(45, 247.5); break;    case 2: setmd(50, 157.5); break;    case 3: setmd(55, 337.5); break;    case 4:setmd(60, 67.5);break;
+        case 1: setp(45, 247.5); break;    case 2: setp(50, 157.5); break;    case 3: setp(55, 337.5); break;    case 4:setp(60, 67.5);break;
       }
     }
     if (record->event.key.col == 2) {
       switch (record->event.key.row) {
-        case 1: setmd(25, 180); break;     case 2: setmd(30, 90); break;    case 3: setmd(35, 270); break;      case 4:setmd(40, 0);break;
+        case 1: setp(25, 180); break;     case 2: setp(30, 90); break;    case 3: setp(35, 270); break;      case 4:setp(40, 0);break;
       }
     }
     if (record->event.key.col == 3) {
       switch (record->event.key.row) {
-        case 1: setmd(3,  225); break;      case 2: setmd(6, 135); break;     case 3: setmd(10, 315); break;   case 4:setmd(20, 45);break;
+        case 1: setp(3,  225); break;      case 2: setp(6, 135); break;     case 3: setp(10, 315); break;   case 4:setp(20, 45);break;
       }
     }
 
@@ -254,10 +254,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
     case MGRID:
-      mouse_keys(record);
+      custom_pointer(record);
       break;
     case KC_ESC:
-      mouseState = 0; // reset mouse pointer
+      stateMouseSequence = 0; // reset mouse pointer
       break;
   }
   return true;
@@ -265,67 +265,46 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 uint32_t layer_state_set_user(uint32_t state) {
     uint8_t layer = biton32(state);
-#ifdef CONSOLE_ENABLE
-  uprintf("_MS_TEST: %u, layer: %u, ms: %u \n\n", _MS_TEST, layer, mouseState);
-#endif
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
-    switch (layer) {
+    if(!disable_layer_color) {
+      rgblight_enable_noeeprom();
+      rgblight_mode_noeeprom(1);
+      switch (layer) {
         case _BAS:
-            if(!disable_layer_color) {
-                rgblight_enable_noeeprom();
-                rgblight_mode_noeeprom(1);
-                rgblight_sethsv_noeeprom(0,252,255);
-            }
-            break;
-        case _LSY:
-        case _RNU:
-            ergodox_right_led_1_on(); // top
-            if(!disable_layer_color) {
-                rgblight_enable_noeeprom();
-                rgblight_mode_noeeprom(1);
-                rgblight_sethsv_noeeprom(7,255,147);
-            }
-            break;
+          rgblight_sethsv_noeeprom(0,252,255);
+          break;
+        case _SYM:
+        case _NUM:
+          ergodox_right_led_1_on(); // top
+          rgblight_sethsv_noeeprom(7,255,147);
+          break;
         case _LMS:
         case _RMS:
-            mouseState = 2;
-            if(!disable_layer_color) {
-                rgblight_enable_noeeprom();
-                rgblight_mode_noeeprom(1);
-                rgblight_sethsv_noeeprom(10,90,40);
-            }
-            break;
+          stateMouseSequence = 2;
+          rgblight_sethsv_noeeprom(10,90,40);
+          break;
         case _FUN:
-            if(!disable_layer_color) {
-                rgblight_enable_noeeprom();
-                rgblight_mode_noeeprom(1);
-                rgblight_sethsv_noeeprom(70,0,50);
-            }
-            break;
-        case _MS_TEST:
-            mouseState = 2;
-            if(!disable_layer_color) {
-                rgblight_enable_noeeprom();
-                rgblight_mode_noeeprom(1);
-                rgblight_sethsv_noeeprom(100,150,100);
-            }
-            break;
+          rgblight_sethsv_noeeprom(70,0,50);
+          break;
+        case _TEST:
+          stateMouseSequence = 2;
+          rgblight_sethsv_noeeprom(100,150,100);
+          break;
         default:
-            if(!disable_layer_color) {
-                rgblight_config.raw = eeconfig_read_rgblight();
-                if(rgblight_config.enable == true) {
-                    rgblight_enable();
-                    rgblight_mode(rgblight_config.mode);
-                    rgblight_sethsv(rgblight_config.hue, rgblight_config.sat, rgblight_config.val);
-                }
-                else {
-                    rgblight_disable();
-                }
-            }
-            break;
+          rgblight_config.raw = eeconfig_read_rgblight();
+          if(rgblight_config.enable == true) {
+            rgblight_enable();
+            rgblight_mode(rgblight_config.mode);
+            rgblight_sethsv(rgblight_config.hue, rgblight_config.sat, rgblight_config.val);
+          }
+          else {
+            rgblight_disable();
+          }
+          break;
+      }
     }
     return state;
 };
